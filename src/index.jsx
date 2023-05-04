56d386524b965163b9e021a04de5981cfcd0f33a
mikelisrael <israelipinkz@gmail.com>
2023-05-04 23:24:50 +0100

    chore: change App and main file name

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./Layout.jsx";
 import "./index.css";
 import { AppProvider } from "./context.jsx";
 
