d091732eb05916f67dceb762d15984bffe764bf4
mikelisrael <israelipinkz@gmail.com>
2023-05-04 12:14:27 +0100

    init: initialize vite react app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-[#d74]">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
