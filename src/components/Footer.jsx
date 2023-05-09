7b1844559187bd777dfe5ce7bd558164f4155157
mikelisrael <israelipinkz@gmail.com>
2023-05-09 16:25:33 +0100

    feat: build footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white py-7">
+      <center>
+        <img src="/logo.svg" className="w-40" alt="logo" />
+        <small className="-mt-2 block">&copy; 2023 ULCERAVE</small>
+      </center>
+    </footer>
+  );
+};
+
+export default Footer;
