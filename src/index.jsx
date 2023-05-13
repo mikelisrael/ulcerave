32c9a566517a16e019000549b532e727200b33cd
mikelisrael <israelipinkz@gmail.com>
2023-05-13 01:17:33 +0100

    style: include css for carousel on dashboard

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./Layout.jsx";
 import "./index.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import { AppProvider } from "./context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
