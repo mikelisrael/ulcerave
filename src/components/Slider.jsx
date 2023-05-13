0840420dbffc8b599b578a1a7fee4ab38f1695f6
mikelisrael <israelipinkz@gmail.com>
2023-05-13 01:15:15 +0100

    feat: include range slider

---
+++ src/components/Slider.jsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Slider from "@mui/material/Slider";
+
+export default function SliderSizes() {
+  return (
+    <Box
+      sx={{
+        width: 300,
+        backgroundColor: "#f6f6f6",
+        padding: 0,
+        borderRadius: 8,
+        display: "flex",
+        alignItems: "center",
+        border: "10px solid white",
+      }}
+    >
+      <Slider
+        sx={{
+          color: "#f6f6f6",
+          "& .MuiSlider-thumb": {
+            backgroundColor: "white",
+            width: 24,
+            height: 24,
+            marginLeft: 2,
+          },
+          "& .MuiSlider-valueLabel": {
+            fontSize: 18,
+            fontWeight: "bold",
+            color: "white",
+            backgroundColor: "white",
+            borderRadius: 8,
+            color: "#808080",
+          },
+        }}
+        size="medium"
+        defaultValue={0}
+        aria-label="Small"
+        valueLabelDisplay="on"
+      />
+    </Box>
+  );
+}
