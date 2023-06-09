dab21ed9b8dc69830902608fd6abecec8c7f1076
mikelisrael <israelipinkz@gmail.com>
2023-06-09 21:53:01 +0100

    feat: build single reminder component

---
+++ src/components/SingleReminder.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import moment from "moment";
+
+const SingleReminder = ({reminder}) => {
+  return (
+    <div className="flex items-center gap-2 rounded-lg border-2 py-2 px-4 md:py-4">
+      <div className="flex-grow space-y-1">
+        <h3 className="font-medium capitalize text-black md:text-lg">
+          {reminder?.category}
+        </h3>
+        <span className="block text-sm font-medium text-grey">
+          {moment(reminder?.date).format("hh:mm A")}
+        </span>
+        <p className="text-sm">
+          {reminder?.description?.length > 50
+            ? reminder?.description?.slice(0, 50) + "..."
+            : reminder?.description}
+        </p>
+      </div>
+
+      <button className="flex-shrink-0 whitespace-nowrap text-xs font-medium text-primaryBlue">
+        See Details
+      </button>
+    </div>
+  );
+}
+
+export default SingleReminder
