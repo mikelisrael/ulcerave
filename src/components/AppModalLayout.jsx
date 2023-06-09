9a1179208f3e17b067fd01141ceed89e575f6960
mikelisrael <israelipinkz@gmail.com>
2023-06-09 21:52:37 +0100

    update: change overflow of modal

--- src/components/AppModalLayout.jsx
+++ src/components/AppModalLayout.jsx
@@ -5,19 +5,13 @@ import { Modal } from "@mui/material";
 const AppModalLayout = ({ open, setOpen, closeFunc, children }) => {
   closeFunc = closeFunc || (() => setOpen(false));
 
-  // TODO: uncomment animation
-
   return (
-    <Modal
-      open={open}
-      onClose={() => setOpen(false)}
-      // hideBackdrop={true}
-    >
-      <div className="bg-mainBg flex h-full w-full items-start justify-center">
+    <Modal open={open} onClose={() => setOpen(false)}>
+      <div className="bg-mainBg flex h-full w-full items-start justify-center overflow-y-auto">
         <div
-          className="relative mt-9 max-h-[80vh] w-[90%] overflow-y-auto rounded-xl bg-white shadow-md md:mt-16 md:w-[60%] lg:w-[50%] xl:w-1/3"
-          // data-aos="zoom-out"
-          // data-aos-duration="500"
+          className="relative mt-9 w-[90%] rounded-xl bg-white shadow-md md:mt-16 md:w-[60%] lg:w-[50%] xl:w-1/3"
+          data-aos="zoom-out"
+          data-aos-duration="500"
         >
           <button
             className="absolute right-4 top-4"
