082e42b4c6e7cc4a9c6d952556a01a7733a8d9e7
mikelisrael <israeipinkz@gmail.com>
2023-06-09 10:37:57 +0100

    wip: build add new traker modal

--- src/components/AddNewTracker.jsx
+++ src/components/AddNewTracker.jsx
@@ -1,7 +1,28 @@
 import moment from "moment";
-import React from "react";
+import React, { useState } from "react";
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 
 const AddNewTracker = () => {
+  const [selectedSymptoms, setSelectedSymptoms] = useState([]);
+  const ulcerSymptoms = [
+    "bloating",
+    "nausea",
+    "vomiting",
+    "heartburn",
+    "indigestion",
+    "abdominal pain",
+  ];
+
+  const handleSymptomChange = (e) => {
+    const value = e.target.value;
+
+    if (selectedSymptoms.includes(value)) {
+      return;
+    } else {
+      setSelectedSymptoms([...selectedSymptoms, value]);
+    }
+  };
+
   return (
     <div className="space-y-5">
       <section className="mb-5 text-center">
@@ -9,6 +30,88 @@ const AddNewTracker = () => {
           New Symptom
         </h1>
         <h3 className="text-grey">{moment().format("Do, MMM YYYY")}</h3>
+      </section>
+
+      <section className="space-y-5">
+        <input
+          type="text"
+          placeholder="Title"
+          className="w-full rounded-md bg-gray-100 px-4 py-3 md:text-lg"
+        />
+
+        <select
+          className="w-full rounded-md border bg-transparent px-4 py-2 md:text-lg"
+          defaultValue=""
+        >
+          <option value="" hidden disabled="disabled">
+            Pain Level
+          </option>
+          <option value="medication">Medication</option>
+          <option value="food">Food</option>
+        </select>
+
+        <div>
+          <select
+            className="w-full rounded-md border bg-transparent px-4 py-2 capitalize md:text-lg"
+            value={
+              selectedSymptoms.length > 0
+                ? selectedSymptoms[selectedSymptoms.length - 1]
+                : ""
+            }
+            onChange={handleSymptomChange}
+          >
+            <option value="" disabled hidden>
+              Symptoms
+            </option>
+            {ulcerSymptoms.map((symptom) => (
+              <option
+                className={`capitalize ${
+                  selectedSymptoms.includes(symptom) && "text-gray-300"
+                }`}
+                key={symptom}
+                value={symptom}
+              >
+                {symptom}
+              </option>
+            ))}
+          </select>
+          {selectedSymptoms.length > 0 && (
+            <ul className="mt-2 flex flex-wrap gap-2">
+              {selectedSymptoms.map((symptom) => (
+                <li
+                  key={symptom}
+                  className="rounded-md border px-3 py-1 text-sm capitalize text-grey"
+                >
+                  {symptom}
+                  <button
+                    className="ml-1"
+                    onClick={() => {
+                      setSelectedSymptoms(
+                        selectedSymptoms.filter((item) => item !== symptom)
+                      );
+                    }}
+                  >
+                    <CloseOutlinedIcon
+                      sx={{
+                        fontSize: "0.8rem",
+                      }}
+                    />
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+
+        <div>
+          <h4 className="text-grey mb-2">Description</h4>
+          <textarea
+            placeholder="Description"
+            className="w-full resize-none rounded-md bg-gray-100 px-4 py-3 md:text-lg"
+            cols={30}
+            rows={5}
+          ></textarea>
+        </div>
       </section>
     </div>
   );
